cf0dcc2b8faa27e275bd6b31948ca4fe9ba8e5bf
GregBe <inderriegel@gmail.com>
2015-03-29 15:56:12 +0200

    #21 implemented the Barcodescanner

--- Edible/src/allergeeks/edible/vuzix_app/MainActivity.java
+++ Edible/src/allergeeks/edible/vuzix_app/MainActivity.java
@@ -1,32 +1,88 @@
 package allergeeks.edible.vuzix_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.vuzix.speech.VoiceControl;
 
 public class MainActivity extends Activity {
 	
 	private Activity main = this;
-	private TextView result, contentTxt;
+	private TextView result, contentTxt, formatTxt;
 	private VoiceControl vc;
 	Toast toast;
+	private Button scanBtn;
+	//private TextView formatTxt, contentTxt;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-	result = (TextView)findViewById(R.id.result);
+		result = (TextView)findViewById(R.id.result);
 		
 		
 		result.setText("Test");
 		vc = new myVoiceControl(this);
 		
+		main = this;
+		scanBtn = (Button)findViewById(R.id.scan_button);
+		formatTxt = (TextView)findViewById(R.id.scan_format);
+		contentTxt = (TextView)findViewById(R.id.scan_content);
+		scanBtn.setOnClickListener(new OnClickListener(){
+			
+			public void onClick(View v){
+				//respond to clicks
+				if(v.getId()==R.id.scan_button){
+					//scan
+					IntentIntegrator scanIntegrator = new IntentIntegrator(main);
+					scanIntegrator.initiateScan();
+					}
+				}
+		});
+	}
+	
+	
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		//retrieve scan result
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		
+		if (scanningResult != null) {
+			//we have a result
+			String scanContent = scanningResult.getContents();
+			int result = Integer.parseInt(scanContent);
+			//String scanFormat = scanningResult.getFormatName();
+			
+			if(result == 42141112){
+				Toast toast =Toast.makeText(getApplicationContext(), "Worked", Toast.LENGTH_SHORT);
+				toast.show();
+				
+			}
+			else 
+			{
+				Toast toast =Toast.makeText(getApplicationContext(), "Didn't Worked", Toast.LENGTH_SHORT);
+				toast.show();
+				
+			}
+			}
+		else{
+		    Toast toast = Toast.makeText(getApplicationContext(), 
+		        "No scan data received!", Toast.LENGTH_SHORT);
+		    toast.show();
+		}
+		}
 		/*if(vc != null){
 			 toast.makeText(this,"klappt",Toast.LENGTH_LONG).show();
 			
@@ -36,7 +92,7 @@ public class MainActivity extends Activity {
 		}
 		*/
 
-	}
+	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,6 +125,12 @@ public class MainActivity extends Activity {
 		vc.off();
 	}
 
+	protected void onDestroy(){
+		super.onDestroy();
+		if(vc != null){
+			vc.destroy();
+		}
+	}
 
 }
 
